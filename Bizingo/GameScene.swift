5b90041e41ac880c922561dd88e6ecf506ace51f
Bia <biaplutarco@gmail.com>
2020-02-03 10:43:27 -0300

    testant tile map

--- Bizingo/GameScene.swift
+++ Bizingo/GameScene.swift
@@ -8,103 +8,57 @@
 
 import SpriteKit
 import GameplayKit
+import UIKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
     override func sceneDidLoad() {
+        drawBoard()
+    }
+    
+    func drawBoard() {
+        // Board parameters
+        let numRows = 11
+        let numCols = 11
+        
+        var collum = [3, 4, 5, 6, 7, 8, 9, 10, 11, 10, 9]
 
-        self.lastUpdateTime = 0
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let xOffset: CGFloat = 100
+        let yOffset: CGFloat = 50
+        // Column characters
+        let alphas: String = "abcdefghikl"
+        // Used to alternate between white and black squares
+        var toggle: Bool = false
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        for row in 0...numRows - 1 {
+            for col in 0...numCols - 1 {
+                
+                collum.forEach { (collumzinho) in
+                    // Letter for this column
+                    let colChar = Array(alphas)[col]
+                    // Determine the color of square
+                    let triangle = SKShapeNode()
+                    let path = UIBezierPath()
+                    path.move(to: CGPoint(x: 0, y: 0))
+                    path.addLine(to: CGPoint(x: 30, y: 52))
+                    path.addLine(to: CGPoint(x: 60, y: 0))
+                    path.close()
+                    triangle.path = path.cgPath
+                    triangle.fillColor = .black
+                    triangle.strokeColor = .clear
+                    triangle.position = CGPoint(x: CGFloat(60 * collumzinho) - 330,
+                                              y: CGFloat(52 * row))
+                    // Set sprite's name (e.g., a8, c5, d1)
+                    triangle.name = "\(colChar)\(2 - row)"
+                    self.addChild(triangle)
+                    toggle = !toggle
+                }
+                
+                
+                
+            }
+            toggle = !toggle
         }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
 }
